734f8cb954016a08fd517808637900dc9f7e11bc
Ashutosh Bharat Utane <ashutoshutane453@gmail.com>
2026-03-11 02:34:13 +0530

    for diff.java

--- diff.java
+++ diff.java
@@ -2,7 +2,7 @@ public class diff {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-
+        System.out.println("Hello");
         System.out.println("sum : "+ (a-b));
     }
 }
